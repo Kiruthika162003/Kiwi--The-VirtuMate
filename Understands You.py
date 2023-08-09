cf460077cc1b183a8f69b95f249af59b32e01108
KIRUTHIKA S <89388434+Kiruthika162003@users.noreply.github.com>
2023-08-09 07:25:30 -0700

    Create Understands You.py

---
+++ Understands You.py
@@ -0,0 +1,28 @@
+from transformers import pipeline, Conversation
+
+class KiwiAssistant:
+    def __init__(self):
+        self.chatbot = pipeline("conversational")
+
+    def communicate(self, user_input):
+        conv = Conversation([{"role": "system", "content": "I am Kiwi, your helpful assistant."}, {"role": "user", "content": user_input}])
+        response = self.chatbot(conv)[-1]["generated"]["text"]
+        return response
+
+def main():
+    print("Kiwi: Hello! I'm Kiwi, your advanced virtual assistant.")
+    print("Kiwi: How can I assist you today? Feel free to ask or command. Type 'exit' to end the conversation.")
+
+    kiwi = KiwiAssistant()
+    user_input = input("You: ")
+
+    while user_input.lower() != "exit":
+        kiwi_response = kiwi.communicate(user_input)
+        print("Kiwi:", kiwi_response)
+
+        user_input = input("You: ")
+
+    print("Kiwi: Goodbye! Have a wonderful day.")
+
+if __name__ == "__main__":
+    main()
